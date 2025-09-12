ac1831971dde1a2b67ca8675210ea108e99241c4
Kevin Ortiz <92760727+KevinorJG@users.noreply.github.com>
2025-09-12 20:13:07 +0000

    Agregando debug de credenciales

--- src/core/persistence/config/data-source.ts
+++ src/core/persistence/config/data-source.ts
@@ -7,6 +7,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const isProd = process.env.NODE_ENV === 'production';
+console.log(JSON.stringify({host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME}))
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
